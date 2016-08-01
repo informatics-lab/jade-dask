830b0d22304e34a48ad28be8aeca314c3b60eb8f
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-01 13:59:31 +0100

    Added config and keys

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -11,7 +11,11 @@ curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose
 chmod +x /usr/local/bin/docker-compose
 usermod -aG docker ec2-user
 
-# curl config
+# get keys
+s3get jade-secrets/jade-secrets
 
+# get config
+git clone https://github.com/met-office-lab/jade.git
 
 # run config
+docker-compose up -d -f jade/docker/master
--- terraform/jade.tf
+++ terraform/jade.tf
@@ -1,13 +1,15 @@
 provider "aws" {
-  region     = "eu-west-1"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "jademaster" {
-  ami           = "ami-f9dd458a"
-  instance_type = "t2.micro"
-  key_name      = "gateway"
-  tags          = {
-      Name = "jademaster"
+  ami                   = "ami-f9dd458a"
+  instance_type         = "t2.micro"
+  key_name              = "gateway"
+  user_data             = "${file("bootstrap/bootstrap.sh")}"
+  iam_instance_profile  = "jade-secrets"
+
+  tags = {
+    Name = "jademaster"
   }
-  user_data     = "${file("bootstrap/bootstrap.sh")}"
 }
