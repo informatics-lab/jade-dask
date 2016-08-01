6de8b8eb842306b85dd1f11d46c06cd90eff0103
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-01 14:49:43 +0100

    Install deps

--- terraform/jade.tf
+++ terraform/jade.tf
@@ -8,8 +8,28 @@ resource "aws_instance" "jademaster" {
   key_name              = "gateway"
   user_data             = "${file("bootstrap/bootstrap.sh")}"
   iam_instance_profile  = "jade-secrets"
+  security_groups        = ["default", "jademaster"]
 
   tags = {
     Name = "jademaster"
+  }
+}
+
+resource "aws_security_group" "jademaster" {
+  name = "jademaster"
+  description = "Allow jade traffic"
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
   }
 }
