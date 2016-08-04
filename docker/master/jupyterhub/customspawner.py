4d4c33cc75ce912d6c76525fb12978652c353e1a
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-04 15:24:03 +0100

    Copy skel directory and add ipython kernel config

--- docker/master/jupyterhub/customspawner.py
+++ docker/master/jupyterhub/customspawner.py
@@ -1,4 +1,3 @@
-import os
 import shutil
 from traitlets import Unicode
 
@@ -19,12 +18,13 @@ class CustomSpawner(DockerSpawner):\
             username=self.user.name
         )
 
-    def make_preexec_fn(self, name):
-        home = self.home_path
-        def preexec():
-            try:
-                self.log.debug("Copying skel to home/{}".format(self.user.name))
-                shutil.copytree('/mnt/jade-notebooks/home/skel', home)
-            except e:
-                print(e)
-        return preexec
+    def create_home_dir(self):
+        try:
+            self.log.debug("Copying skel to home/{}".format(self.user.name))
+            shutil.copytree('/mnt/jade-notebooks/home/skel', self.home_path)
+        except Exception as e:
+            self.log.error(e)
+
+    def start(self, *args, **kwargs):
+        self.create_home_dir()
+        return super(CustomSpawner, self).start(*args, **kwargs)
