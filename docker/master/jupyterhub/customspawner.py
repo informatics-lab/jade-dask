e79f44c2cfba80d7266c774bd3fdc97838588420
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-05 11:43:04 +0100

    Attempt to lookup swarm node

--- docker/master/jupyterhub/customspawner.py
+++ docker/master/jupyterhub/customspawner.py
@@ -1,10 +1,23 @@
 import shutil
+from tornado import gen
 from traitlets import Unicode
 
 from dockerspawner import DockerSpawner
 
 
-class CustomSpawner(DockerSpawner):\
+class CustomSpawner(DockerSpawner):
+
+    container_ip = '0.0.0.0'
+
+    @gen.coroutine
+    def lookup_node_name(self):
+        """Find the name of the swarm node that the container is running on."""
+        containers = yield self.docker('containers', all=True)
+        for container in containers:
+            if container['Id'] == self.container_id:
+                name, = container['Names']
+                node, container_name = name.lstrip("/").split("/")
+                raise gen.Return(node)
 
     home_path_template = Unicode(
         '/mnt/jade-notebooks/home/{username}',
@@ -26,7 +39,24 @@ class CustomSpawner(DockerSpawner):\
             self.log.error(e)
 
     def start(self, *args, **kwargs):
+        # look up mapping of node names to ip addresses
+        info = yield self.docker('info')
+        num_nodes = int(info['DriverStatus'][3][1])
+        node_info = info['DriverStatus'][4::5]
+        self.node_info = {}
+        for i in range(num_nodes):
+            node, ip_port = node_info[i]
+            self.node_info[node] = ip_port.split(":")[0]
+        self.log.debug("Swarm nodes are: {}".format(self.node_info))
+
+        # create user home directory
         self.create_home_dir()
-        spawned = super(CustomSpawner, self).start(*args, **kwargs)
-        self.log.debug(spawned)
-        return spawned
+
+        # start the container
+        yield super(CustomSpawner, self).start(*args, **kwargs)
+
+        # figure out what the node is and then get its ip
+        name = yield self.lookup_node_name()
+        self.user.server.ip = self.node_info[name]
+        self.log.info("{} was started on {} ({}:{})".format(
+            self.container_name, name, self.user.server.ip, self.user.server.port))
