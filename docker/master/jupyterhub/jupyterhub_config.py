2b8a95785036b2fd74edf0a2bf7aa822a4aec73a
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-08-16 15:04:32 +0100

    Deps for mysql and remove hardcoded admins

--- docker/master/jupyterhub/jupyterhub_config.py
+++ docker/master/jupyterhub/jupyterhub_config.py
@@ -9,7 +9,7 @@ c.JupyterHub.port = 8000
 c.JupyterHub.hub_ip = '0.0.0.0'
 
 # store the sqlite database on persistant storage
-c.JupyterHub.db_url = "mysql://{}:{}@mysql/jupyterhub".format("root", os.environ['MYSQL_ROOT_PASSWORD'])
+c.JupyterHub.db_url = "mysql://{}:{}@mysql/jupyter".format("root", os.environ['MYSQL_ROOT_PASSWORD'])
 
 # use GitHub OAuthenticator for local users
 c.JupyterHub.authenticator_class = 'oauthenticator.LocalGitHubOAuthenticator'
@@ -24,7 +24,3 @@ c.DockerSpawner.container_image = 'quay.io/informaticslab/atmossci-notebook'
 c.DockerSpawner.volumes = {'/mnt/jade-notebooks/home/{username}': '/usr/local/share/notebooks'}
 c.DockerSpawner.hub_ip_connect = requests.get("http://169.254.169.254/latest/meta-data/local-ipv4").text
 c.DockerSpawner.remove_containers = True
-
-# specify users and admin
-c.Authenticator.whitelist = {'niallrobinson', 'jacobtomlinson'}
-c.Authenticator.admin_users = {'niallrobinson', 'jacobtomlinson'}
