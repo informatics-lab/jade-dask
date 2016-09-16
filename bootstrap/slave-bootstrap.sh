1a235828fdfb08c81cf971bad84433edc3b40708
Niall Robinson <niall.h.robinson@gmail.com>
2016-09-16 14:17:48 +0100

    changed to new docker container

--- bootstrap/slave-bootstrap.sh
+++ bootstrap/slave-bootstrap.sh
@@ -33,7 +33,7 @@ git clone https://github.com/met-office-lab/jade.git /usr/local/share/jade
 aws s3 cp s3://jade-secrets/jade-secrets /usr/local/share/jade/jade-secrets
 
 # pull scientific environment image
-docker pull quay.io/informaticslab/atmossci-notebook
+docker pull quay.io/informaticslab/asn-serve
 
 # run config
 docker run -d --add-host "jupyterhub:${jademaster_private_ip}" swarm join --advertise=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'):2375 consul://jupyterhub:8500
