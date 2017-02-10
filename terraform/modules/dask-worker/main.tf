8c0030259e73d905e3bdbfd595947f6e79c41f3d
Theo McCaie <theo.mccaie@informaticslab.co.uk>
2017-02-10 13:16:29 +0000

    update container version

--- terraform/modules/dask-worker/main.tf
+++ terraform/modules/dask-worker/main.tf
@@ -8,7 +8,7 @@ docker run
 --device /dev/fuse
 --cap-add MKNOD
 --entrypoint /bin/bash
-quay.io/informaticslab/asn-serve:v1.0.0 -c
+quay.io/informaticslab/asn-serve:v1.0.1 -c
 "mkdir -p /usr/local/share/notebooks/data/mogreps &&
 s3fs mogreps /usr/local/share/notebooks/data/mogreps -o iam_role=jade-secrets &&
 mkdir -p /usr/local/share/notebooks/data/mogreps-g &&
