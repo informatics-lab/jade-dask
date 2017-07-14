3245752a4116aa02992cbb2eb496bd0608099775
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-07-14 11:41:47 +0100

    Add service account for scaling workers

--- kubernetes/adaptive.py
+++ kubernetes/adaptive.py
@@ -1,9 +1,22 @@
+import logging
+import os
+
 from distributed.deploy import Adaptive
-# from kubernetes import client, config
-from tornado import gen
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
 
-class MyCluster(object):
+logger = logging.getLogger("distributed.deploy.adaptive")
+
+
+class KubeCluster(object):
+    def __init__(self, **kwargs):
+        config.load_incluster_config()
+        self.api_instance = client.ExtensionsV1beta1Api()
+        self.namespace = 'dask'
+        self.deployment = 'dask-workers'
+        logger.info("Initialising kubernetes scaler")
+
     def scale_up(self, n):
         """
         Bring the total count of workers up to ``n``
@@ -13,8 +26,16 @@ class MyCluster(object):
 
         This can be implemented either as a function or as a Tornado coroutine.
         """
-        pass
-
+        logger.info("Scaling up")
+        current_deployment = self.api_instance.read_namespaced_deployments_scale(self.deployment, self.namespace)
+        logger.info("Current number of workers is %s", current_deployment.spec.replicas)
+        current_deployment.spec.replicas = n
+        logger.info("Scaling to %s}", current_deployment.spec.replicas)
+        try:
+            self.api_instance.replace_namespaced_deployments_scale(
+                self.deployment, self.namespace, current_deployment)
+        except ApiException as e:
+            logger.error("Exception when scaling up {}: {}".format(self.deployment, e))
 
     def scale_down(self, workers):
         """
@@ -26,9 +47,21 @@ class MyCluster(object):
 
         This can be implemented either as a function or as a Tornado coroutine.
         """
-        pass
+        logger.info("Scaling down")
+        # if workers:
+        current_deployment = self.api_instance.read_namespaced_deployments_scale(self.deployment, self.namespace)
+        if current_deployment.spec.replicas is None:
+            current_deployment.spec.replicas = 0
+        logger.info("Current number of workers is %s", current_deployment.spec.replicas)
+        current_deployment.spec.replicas = current_deployment.spec.replicas - len(workers)
+        logger.info("Scaling to %s", current_deployment.spec.replicas)
+        try:
+            self.api_instance.replace_namespaced_deployments_scale(
+                self.deployment, self.namespace, current_deployment)
+        except ApiException as e:
+            logger.error("Exception when scaling up {}: {}".format(self.deployment, e))
 
 
 def dask_setup(scheduler):
-    cluster = MyCluster()
-    # adapative_cluster = Adaptive(scheduler, cluster)
+    cluster = KubeCluster()
+    adapative_cluster = Adaptive(scheduler, cluster)
